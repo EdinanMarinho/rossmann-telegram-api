6226cc491a39938374068826cab83a766233b49f
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-10-14 19:00:15 -0300

    Atualizacao codigo

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -18,86 +18,92 @@ TOKEN = os.environ.get('TOKEN')
 # Webhook                                                                                                                        
 #https://api.telegram.org/bot7694499169:AAHUk3YGF-pOq6TephY18BLysH-SZv-PMaM/setWebhook?url=https://3098219175b4bb.lhr.life
 
-# Webhook Renderi
-#https://api.telegram.org/bot7694499169:AAHUk3YGF-pOq6TephY18BLysH-SZv-PMaM/setWebhook?url=https://telegram-bot-rossmann-b1ht.onrender.com/rossmann/predict
+# Webhook Render
+#https://api.telegram.org/bot7694499169:AAHUk3YGF-pOq6TephY18BLysH-SZv-PMaM/setWebhook?url=https://rossmann-telegram-api-jhca.onrender.com
 
 #Message
 #https://api.telegram.org/bot7694499169:AAHUk3YGF-pOq6TephY18BLysH-SZv-PMaM/sendMessage?chat_id=895518040&text=Olá Edinan, eu estou bem, obrigado!
 
-def send_message(chat_id, text):
-    # send message
+def send_message( chat_id, text ):
     url = 'https://api.telegram.org/bot{}/'.format( TOKEN )
     url = url + 'sendMessage?chat_id={}'.format( chat_id )
-
-    r = requests.post( url, json={'text': text} )
-    print('Status code {}'.format( r.status_code ) )
-
+    
+    # API request using POST method 
+    r = requests.post( url, json = {'text': text} )
+    print( 'Status Code {}'.format( r.status_code ) )
+    
     return None
+      
+def load_dataset( store_id ):
+    # loading test dataset
+    #df10 = pd.read_csv( 'C:\\Users\\Antonio Richard\\OneDrive - acad.ifma.edu.br\\Documentos\\A Sala de aprendizado\\DS Library Books\\Meiga Material\\DS_Producao\\DS\\dataset\\test.csv' )
+    #df_store_raw = pd.read_csv( 'C:\\Users\\Antonio Richard\\OneDrive - acad.ifma.edu.br\\Documentos\\A Sala de aprendizado\\DS Library Books\\Meiga Material\\DS_Producao\\DS\\dataset\\store.csv' )
 
+    df10 = pd.read_csv( 'test.csv' )
+    df_store_raw = pd.read_csv( 'store.csv' )
 
-def load_dataset(store_id):
-    df10 = pd.read_csv('test.csv', low_memory=False )
-    df_store_raw = pd.read_csv('store.csv', low_memory=False)
+    # merge test dataset + store (para ter as mesmas features usadas para fazer as predicoes)
+    df_test = pd.merge( df10, df_store_raw, how = 'left', on = 'Store' )
 
-    # merge test dataset + store (contendo as mesmas features usadas para fazer as predicoes)
-    df_test = pd.merge( df10, df_store_raw, how='left', on='Store' )
-
-    # escolher store para predicao
+    # escolha uma loja 
     df_test = df_test[df_test['Store'] == store_id ]
-
+    
     if not df_test.empty:
-
-        # remover dias fechados
-        df_test = df_test[df_test['Open'] != 0]
-        df_test = df_test[~df_test['Open'].isnull()]    
-        df_test = df_test.drop( 'Id', axis=1 )
-
-        # Convertendo DataFrame em JSON
-        data = json.dumps( df_test.to_dict( orient='records' ))
+        # somente as lojas que estao abertas
+        df_test = df_test[df_test['Open'] == 1]
+        # lojas sem dados faltantes na coluna 'Open'
+        df_test = df_test[ ~df_test['Open'].isnull() ]
+        # retira a coluna 'Id' 
+        df_test = df_test.drop( 'Id', axis = 1 )
+    
+        # converte o DataFrame em json para o envio via API
+        data = json.dumps( df_test.to_dict( orient = 'records' ) )
     
     else:
         data = 'error'
-
+        
     return data
 
 def predict( data ):
-    # API Call
-    url = 'https://rossmann-telegram-api-jhca.onrender.com/rossmann/predict' 
-    header = {'Content-type': 'application/json'}
+    # Chamada para a API
+    url ='https://rossmann-telegram-api-jhca.onrender.com/rossmann/predict'
+    # indica para a API o tipo de requisicao que estamos fazendo
+    header = {'Content-type' : 'application/json'}
+    # dado enviado
     data = data
-    
+
     # requisicao
-    r = requests.post( url, data = data,headers = header )
+    r = requests.post( url, data = data, headers = header )
     print( 'Status Code {}'.format( r.status_code ) )
 
     # cria um objeto DataFrame a partir da lista de dicionarios
-    d1 = pd.DataFrame( r.json(), columns=r.json()[0].keys() )
-
+    d1 = pd.DataFrame( r.json(), columns = r.json()[0].keys() )
+    
     return d1
 
 def parse_message( message ):
     chat_id = message['message']['chat']['id']
     store_id = message['message']['text']
     
-    store_id = store_id.replace( '/', '')
+    store_id = store_id.replace( '/', '' )
     
     try:
         store_id = int( store_id )
+        
     except ValueError:
         store_id = 'error'
-
+        
     return chat_id, store_id
 
 # API initialize
-app = Flask(__name__)
+app = Flask( __name__ )
 
-@app.route('/', methods=['GET', 'POST'] )
+@app.route( '/', methods = ['GET', 'POST'] )
 def index():
     if request.method == 'POST':
         message = request.get_json()
+        chat_id, store_id = parse_message( message )
         
-        chat_id, store_id = parse_message(message)
-
         if store_id != 'error':
             # loading data
             data = load_dataset( store_id )
@@ -105,32 +111,31 @@ def index():
             if data != 'error':
                 # prediction
                 d1 = predict( data )
-            
-                # calculation
-                d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
 
-                # send message
-                msg = 'Loja número {} venderá R$ {:,.2f} nas próximas 6 semanas'.format(
-                        d2['store'].values[0],
-                        d2['prediction'].values[0] )          
+                # Calculation
+                # DataFrame que contem a soma das previsoes de vendas por loja
+                d2 = d1[['store','prediction']].groupby(['store']).sum().reset_index()
                 
+                msg = 'Store number {} will sell R${:,.2f} in the next six weeks'.format(
+                                                                                         d2['store'].values[0],
+                                                                                         d2['prediction'].values[0]
+                                                                                        )
                 send_message( chat_id, msg )
-                return Response( 'OK', status=200 )  
+                return Response( 'Ok', status = 200 )
             
-            else:
-                send_message( chat_id, 'Não existe esse {} código de loja'.format(d2['store'].values[0]) )
-                return Response( 'OK', status=200)
-
+            else: 
+                send_message( chat_id, 'Store Not Available' )
+                return Response( 'Ok', status = 200 )
+        
         else:
-            send_message( chat_id, 'Número da Loja está Errado')
-            return Response( 'OK', status=200 ) 
-
-
+            send_message( chat_id, 'Store ID Wrong' )
+            return Response( 'Ok', status = 200 ) 
+        
     else:
         return '<h1> Rossmann Telegram BOT </h1>'
     
-
+    
 if __name__ == '__main__':
+    # o servidor Flask eh iniciado para escutar a API
     port = os.environ.get( 'PORT', 5000 )
-    app.run( host='0.0.0.0', port=port , debug=True )
-
+    app.run( host='0.0.0.0', port=port, debug = True )
