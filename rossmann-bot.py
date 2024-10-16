7506052df3f49b3dd821ce3fab23f1f586390d77
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-10-16 09:57:32 -0300

    Alterado PORT & host

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -132,4 +132,4 @@ def index():
     
 if __name__ == '__main__':
     port = os.environ.get( 'PORT', 10000 )
-    app.run( host='0.0.0.0', port=port, debug=True )
+    app.run( host='127.0.0.1', port=port, debug=True )
