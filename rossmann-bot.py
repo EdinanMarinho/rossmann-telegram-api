036317dd61f6bc3b45011847b0e9fb46921491be
EdinanMarinho <61295729+EdinanMarinho@users.noreply.github.com>
2024-10-18 18:00:17 -0300

    Inserido parametro timeout no request

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -65,7 +65,7 @@ def predict( data ):
     header = {'Content-type': 'application/json' }
     data = data
 
-    r = requests.post( url, data=data, headers=header )
+    r = requests.post( url, data=data, headers=header, timeout=120 )
     print( 'Status Code {}'.format( r.status_code ) )
 
     d1 = pd.DataFrame( r.json(), columns=r.json()[0].keys() )
